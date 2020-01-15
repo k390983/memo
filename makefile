3c4a9e7cd1faccf9ac31a176d98c3ef9735cb863
kai <kaikitagawajones@gmail.com>
2020-01-15 18:03:50 +0900

    raw i/o

--- makefile
+++ makefile
@@ -3,11 +3,12 @@ CC = gcc
 
 CFLAGS = -Wall
 LFLAGS =
+STD = -std=c99
 
 TARGET = memo
 
 all:
-	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c $(LFLAGS)
+	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c $(LFLAGS) $(STD)
 
 clean:
 	$(RM) $(TARGET)
--- memo.c
+++ memo.c
@@ -1,6 +1,6 @@
 /*
-https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html
-step 42
+https://viewsourcecode.org/snaptoken/kilo/04.aTextViewer.html
+step 54
 */
 
 //---- includes ----
@@ -20,9 +20,23 @@ step 42
 #define ABUF_INIT {NULL, 0}
 #define VERSION "0.0.1"
 
+enum editorKey {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN,
+	HOME_KEY,
+	END_KEY,
+	DEL_KEY
+
+};
+
 // ---- structs ----
 
 struct editorConfig{
+	int cx, cy;
 	int screenrows;
 	int screencols;
  	struct termios orig_termios;
@@ -74,7 +88,7 @@ void enableRawMode() {
 
 }
 
-char editorReadKey() {
+int editorReadKey(){
 	int nread;
 	char c;
 
@@ -83,7 +97,61 @@ char editorReadKey() {
 			die("read");
 	}
 
-	return c;
+	if(c == '\x1b'){
+		char seq[3];
+		if(read(STDIN_FILENO, &seq[0], 1) != 1)
+			return '\x1b';
+
+		if(read(STDIN_FILENO, &seq[1], 1) != 1)
+			return '\x1b';
+
+		if(seq[0] == '['){
+			if(seq[1] >= '0' && seq[1] <= '9'){
+				if(read(STDIN_FILENO, &seq[2], 1) != 1)
+					return '\x1b';
+
+				if(seq[2] == '~'){
+					switch(seq[1]){
+						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+
+					}
+
+				}
+
+			}else{
+				switch(seq[1]){
+					case 'A': return ARROW_UP;
+			        case 'B': return ARROW_DOWN;
+			        case 'C': return ARROW_RIGHT;
+			        case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+          			case 'F': return END_KEY;
+
+				}
+
+			}
+
+		}else if(seq[0] == 'O'){
+			switch(seq[1]){
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+
+			}
+
+		}
+
+		return '\x1b';
+
+	}else{
+		return c;
+
+	}
 
 }
 
@@ -153,14 +221,68 @@ void abFree(struct abuf *ab){
 
 // ---- input ----
 
+void editorMoveCursor(int key){
+	switch (key) {
+		case ARROW_LEFT:
+			if(E.cx != 0)
+				E.cx--;
+
+			break;
+
+		case ARROW_RIGHT:
+			if(E.cx != E.screencols - 1)
+				E.cx++;
+
+			break;
+
+		case ARROW_UP:
+			if(E.cy != 0)
+				E.cy--;
+
+			break;
+
+		case ARROW_DOWN:
+			if(E.cy != E.screenrows - 1)
+				E.cy++;
+
+			break;
+
+	}
+
+}
+
 void editorProcessKeypress(){
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+			int times = E.screenrows;
+			while (times--)
+			editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
+			break;
+
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
+			editorMoveCursor(c);
 			break;
 
 	}
@@ -217,7 +339,10 @@ void editorRefreshScreen(){
 
 	editorDrawRows(&ab);//draw
 
-	abAppend(&ab, "\x1b[H", 3);
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	abAppend(&ab, buf, strlen(buf));
+
 	abAppend(&ab, "\x1b[?25h", 6);//show cursor
 
 	write(STDOUT_FILENO, ab.b, ab.len);
@@ -229,6 +354,9 @@ void editorRefreshScreen(){
 // ---- main ----
 
 void initEditor(){
+	E.cx = 0;
+	E.cy = 0;
+
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
 }
